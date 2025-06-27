5ff791c96876d74d9c9d9524106102a071621596
fyosua <ferdianyosua@gmail.com>
2025-06-28 00:15:50 +0800

    fix ThemeProvider

--- src/app/components/ThemeProvider.tsx
+++ src/app/components/ThemeProvider.tsx
@@ -3,6 +3,7 @@
 
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+// The import path below is corrected
 import { type ThemeProviderProps } from 'next-themes/dist/types'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
