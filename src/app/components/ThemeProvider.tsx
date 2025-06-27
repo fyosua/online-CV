1a169f709bf7072281b2093221f26ff52d7c75e6
fyosua <ferdianyosua@gmail.com>
2025-06-28 00:19:44 +0800

    fix ThemeProvider

--- src/app/components/ThemeProvider.tsx
+++ src/app/components/ThemeProvider.tsx
@@ -2,10 +2,8 @@
 
 import { useState, useEffect } from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-// Import the props type from the library
-import { type ThemeProviderProps } from 'next-themes/dist/types'
+import { type ThemeProviderProps } from 'next-themes'
 
-// Update the function to accept props
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) { 
   const [mounted, setMounted] = useState(false)
 
@@ -17,6 +15,5 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     return null
   }
 
-  // Pass the received props down to the provider
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
